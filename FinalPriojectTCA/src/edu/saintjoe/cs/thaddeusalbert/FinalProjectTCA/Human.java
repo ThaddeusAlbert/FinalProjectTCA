4c285c11bb904083d800e7d18e458e68f1d036ab
thaddeusalbert <talbert@saintjoe.edu>
2015-05-06 15:46:26 -0400

    initial commit with human errorss

--- FinalPriojectTCA/src/edu/saintjoe/cs/thaddeusalbert/FinalProjectTCA/Human.java
+++ FinalPriojectTCA/src/edu/saintjoe/cs/thaddeusalbert/FinalProjectTCA/Human.java
@@ -1,5 +1,23 @@
 package edu.saintjoe.cs.thaddeusalbert.FinalProjectTCA;
 
-public class Human {
-
+public class Human extends Mammal{
+	public Human(String name, Object size){
+		super(name, size)
+	}
+	public Human(String name, Mammal Size){
+		super(name, size.toString());
+		size = huge;
+	}
+	public Object setValue(Object newSize) {
+		Object retval = super.setValue(newValue);
+		return retval;
+		}
+	public MammalSize setValue(MammalSize newValue);
+	super.setValue(newValue.toString());
+	size = huge;
+	return oldvalue;
+}
+public MammalSize getValue(){
+	return size;
+}
 }
