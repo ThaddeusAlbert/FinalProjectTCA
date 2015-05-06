6156cfefc4597c9eb7be4ffcb575dcde54b5f27e
thaddeusalbert <talbert@saintjoe.edu>
2015-05-06 15:55:59 -0400

    final committ

--- FinalPriojectTCA/src/edu/saintjoe/cs/thaddeusalbert/FinalProjectTCA/FinalProjectTCADriver.java
+++ FinalPriojectTCA/src/edu/saintjoe/cs/thaddeusalbert/FinalProjectTCA/FinalProjectTCADriver.java
@@ -1,5 +1,14 @@
 package edu.saintjoe.cs.thaddeusalbert.FinalProjectTCA;
 
 public class FinalProjectTCADriver {
-
+	public static void main(String[] args){
+		FinalProjectTCA MammalOne, MammalTwo, MammalThree;
+		MammalOne = new FinalProjectTCA("mouse", 13, 1);
+		MammalTwo = new FinalProjectTCA("lamb", 4, 2);
+		MammalThree = new FinalProjectTCA("Horse", 15, 3);
+	
+		System.out.println("Data for mammal one: "+ MammalOne.toString());
+		System.out.println("Data for mammal two: "+ MammalTwo.toString());
+		System.out.println("Data for mammal three: "+ MammalThree.toString());
+		}
 }
